dc061cc186bd5108983ca3bf836709157fbd41f5
Travis H <t3patterson@gmail.com>
2017-09-24 20:21:13 -0500

    Create install-ruby-rbenv.sh

---
+++ install-ruby-rbenv.sh
@@ -0,0 +1,13 @@
+cd ~
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.zshrc
+exec $SHELL
+
+rbenv install 2.4.2
+rbenv global 2.4.2
+ruby -v
